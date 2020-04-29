247e235ff3d4cae70da5d0afa87d66d58ddc3af8
Aldrin James Basa <aldrin.basa97@gmail.com>
2020-04-29 11:53:05 -0700

    Started battle framework

---
+++ Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WIN, DEFEAT }
+public class BattleSystem : MonoBehaviour
+{
+
+    public BattleState currentState;
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    Humanoid playerHumanoid;
+    Humanoid enemyHumanoid;
+
+    void Start()
+    {
+        currentState = BattleState.START;
+    }
+
+    IEnumerator StartBattle()
+    {
+        yield return new WaitForSeconds(2f);
+        currentState = BattleState.PLAYERTURN;
+        PlayerTurn();
+    }
+
+    IEnumerator Attack()
+    {
+        bool isDead = enemyHumanoid.DamageTaken(playerHumanoid.damage);
+
+        //Update Enemy HealthBar
+
+        /////
+        
+        yield return new WaitForSeconds(2f);
+        if (isDead)
+        {
+            currentState = BattleState.WIN;
+            EndBattle();
+        }
+        else
+        {
+            currentState = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    void PlayerTurn()
+    {
+
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        yield return new WaitForSeconds(2f);
+
+        //Enemy Attack
+        bool isDead = playerHumanoid.DamageTaken(enemyHumanoid.damage);
+        //Update Player Health Bar
+
+        //////
+        yield return new WaitForSeconds(1f);
+        if (isDead)
+        {
+            currentState = BattleState.DEFEAT;
+            EndBattle();
+        }
+        else
+        {
+            currentState = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+    }
+
+
+    public void OnAttack()
+    {
+        if(currentState != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+
+        StartCoroutine(Attack());
+    }
+
+    void EndBattle()
+    {
+        if(currentState == BattleState.WIN)
+        {
+            //Destroy Enemy
+            //Reward EXP
+        }
+        else if(currentState == BattleState.DEFEAT)
+        {
+            //Stuff to put when you lose
+        }
+    }
+}
---
+++ Assets/Scripts/Humanoid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Humanoid : MonoBehaviour
+{
+
+    public int level;
+    public int damage;
+    public int currentHealth;
+    public int maxHealth;
+
+    public bool DamageTaken(int damageTaken)
+    {
+        currentHealth -= damageTaken;
+
+        if(currentHealth <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+
+}
