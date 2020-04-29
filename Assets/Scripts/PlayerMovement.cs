8adf53f5707df6bf845bc3bd198f48de8798872b
Aldrin James Basa <aldrin.basa97@gmail.com>
2020-04-29 04:07:02 -0700

    Created testing environment for player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public LayerMask movementLayer;
+    private NavMeshAgent playerNavMeshAgent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerNavMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit clickPosition;
+            Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            if(Physics.Raycast(clickRay, out clickPosition, 300, movementLayer))
+            {
+                playerNavMeshAgent.SetDestination(clickPosition.point);
+            }
+        }
+    }
+}
