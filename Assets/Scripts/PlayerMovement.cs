7a9ef0ea10f4a28d4c7eee837b1f8012e8339f24
Aldrin James Basa <aldrin.basa97@gmail.com>
2020-05-04 18:51:46 -0700

    Added battle transition, needs to be disabled as to not obstruct scene view

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     public LayerMask movementLayer;
     private NavMeshAgent playerNavMeshAgent;
+    public Animator transitionAnimator;
 
 
     // Start is called before the first frame update
@@ -29,5 +31,22 @@ public class PlayerMovement : MonoBehaviour
                 playerNavMeshAgent.SetDestination(clickPosition.point);
             }
         }
+    }
+
+    IEnumerator BattleTransition(int levelIndex)
+    {
+        transitionAnimator.SetTrigger("StartBattleTransition");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(levelIndex);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Player entered trigger");
+        if(other.tag == "Enemy")
+        {
+            StartCoroutine(BattleTransition(1));
+        }
+        
     }
 }
